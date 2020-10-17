374cf8460a0095ed6de34cc1f71166bb76c17391
Arkadiusz Ciesielski <arkadiusz.ciesielski@poczta.onet.pl>
2020-10-17 17:59:05 +0200

    Add custom fonts

--- components/shop/ProductItem.js
+++ components/shop/ProductItem.js
@@ -83,11 +83,13 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 18,
-    marginVertical: 4,
+    marginVertical: 2,
+    fontFamily: 'open-sans-bold',
   },
   price: {
     fontSize: 14,
     color: '#888',
+    fontFamily: 'open-sans',
   },
   buttons: {
     flexDirection: 'row',
