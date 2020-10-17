892634fe4e61829155b871cc9dc8b3441ac65ea7
Arkadiusz Ciesielski <arkadiusz.ciesielski@poczta.onet.pl>
2020-10-17 13:54:07 +0200

    Set up navigator

--- App.js
+++ App.js
@@ -1,10 +1,9 @@
-import { StatusBar } from 'expo-status-bar'
 import React from 'react'
 import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
-import { StyleSheet, Text, View } from 'react-native'
 
 import productsReducer from './store/reducers/productsReducer'
+import ShopNavigator from './navigation/ShopNavigator'
 
 const rootReducer = combineReducers({
   products: productsReducer,
@@ -15,19 +14,7 @@ const store = createStore(rootReducer)
 export default function App() {
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style='auto' />
-      </View>
+      <ShopNavigator />
     </Provider>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-})
