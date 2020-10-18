aa90e945df3c60716c72fb445ff249f76fb44aeb
Arkadiusz Ciesielski <arkadiusz.ciesielski@poczta.onet.pl>
2020-10-18 13:53:16 +0200

    Clean the cart after making the order

--- store/reducers/orderReducer.js
+++ store/reducers/orderReducer.js
@@ -1,4 +1,5 @@
 import { ADD_ORDER } from '../actions/orderActions'
+import Order from '../../models/order'
 
 const initialState = {
   orders: [],
@@ -8,9 +9,9 @@ export default (state = initialState, action) => {
   switch (action.type) {
     case ADD_ORDER:
       const newOrder = new Order(
-        new Date.toString(),
+        new Date().toString(),
         action.payload.items,
-        action.payload.amount,
+        action.payload.amount.toFixed(2),
         new Date()
       )
       return {
