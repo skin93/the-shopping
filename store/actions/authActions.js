22a0f65f8903ea846a4dfc9404c30149cb9dc6ff
Arkadiusz Ciesielski <arkadiusz.ciesielski@poczta.onet.pl>
2020-10-20 19:35:36 +0200

    Register user

---
+++ store/actions/authActions.js
@@ -0,0 +1,28 @@
+export const SIGNUP = 'SIGNUP'
+
+export const signup = (email, password) => async (dispatch) => {
+  const res = await fetch(
+    `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCgqW1SQ-eNkX6mGvQxeJJbJ8ECrWvrstk
+    `,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email,
+        password,
+        returnSecureToken: true,
+      }),
+    }
+  )
+
+  if (!res.ok) {
+    throw new Error('Something went wrong!')
+  }
+
+  const data = await res.json()
+  console.log(data)
+
+  dispatch({ type: SIGNUP })
+}
